4fdb017ef60127a5f3bdba41f6dcb51b0255e62a
MollyZen <denis.saltikov@gmail.com>
2024-11-08 23:26:54 +0300

    Fix script not waiting for responses

--- vk_save_album.user.js
+++ vk_save_album.user.js
@@ -50,9 +50,9 @@ var openNew = function (title, content) {
   const link = document.createElement("a");
   const file = new Blob([html], { type: 'text/html' });
   link.href = URL.createObjectURL(file);
-	link.download = title + ".html";
-	link.click();
-	URL.revokeObjectURL(link.href);
+  link.download = title + ".html";
+  link.click();
+  URL.revokeObjectURL(link.href);
 };
 $(document).ready(function () {
   var data = window.location.toString().match(/^https:\/\/vk\.com\/album(\-?[0-9]+)\_([0-9]+)\??.*?$/);
@@ -69,46 +69,58 @@ $(document).on('click', '.vkopt-save', function () {
   var count = 0;
   var all = false;
   var offset = 0;
-  while (count === 0 || !all) {
-    offset = loop * 1000;
-    var html;
-    GM.xmlHttpRequest({
-      synchronous: true,
-      method: 'GET',
-      url: 'https://api.vk.com/method/photos.get?owner_id=' + owner + '&album_id=' + albumId + '&count=1000' + '&offset=' + offset + '&v=5.81' + '&access_token=' + token,
-			onload: function(response){
-        html = response.responseText;
-      }
-    });
-    var json = $.parseJSON(html);
-    if (json.hasOwnProperty('error')) {
-      el.html('[error: ' + json.error.error_msg + ']');
-      return false;
+  var last_response;
+  var need_next = true;
+  function process_queue(){
+    if (all) {
+        el.html('<a class="vkopt-open-page">[' + phrases.open_page + ']</a><a class="vkopt-open-links">[' + phrases.get_links + ']</a>');
+        $(document).on('click', '.vkopt-open-page', function () {
+        openNew($('.photos_album_intro h1').html() + ' (owner id ' + owner + ', album id ' + album + ')', images);
+        return false;
+      });
+      $(document).on('click', '.vkopt-open-links', function () {
+        openNew($('.photos_album_intro h1').html() + ' (owner id ' + owner + ', album id' + album + ')', links);
+        return false;
+      });
+    } else if (need_next) {
+        GM.xmlHttpRequest({
+            synchronous: true,
+            method: 'GET',
+            url: 'https://api.vk.com/method/photos.get?owner_id=' + owner + '&album_id=' + albumId + '&count=1000' + '&offset=' + offset + '&v=5.81' + '&access_token=' + token,
+                  onload: function(response){
+              last_response = response.responseText;
+            }
+          });
+          need_next = false;
+          setTimeout(process_queue,100);
+    } else if (!last_response) {
+      setTimeout(process_queue,100);
+    } else {
+        var json = $.parseJSON(last_response);
+        if (json.hasOwnProperty('error')) {
+          el.html('[error: ' + json.error.error_msg + ']');
+          return false;
+        }
+        var items = json['response']['items'];
+        var url,
+        date;
+        for (id in items) {
+          el.html('[' + phrases.wait + ' ' + count + '/' + json.response.count + ']');
+          url = items[id].sizes.slice(-1)[0].url;
+          date = settings.date.type ? new Date(items[id]['date'] * 1000).toString(settings.date.format)  : '';
+          if (settings.date.type == 2) images += date + '<br/>';
+          images += '<img alt="' + date + '" title="' + date + '" src="' + url + '" />';
+          if (settings.date.type == 2) images += '<br/>';
+          links += url + '<br/>';
+          count++;
+        }
+        if (count >= json.response.count) all = true;
+        loop++;
+        offset = loop * 1000;
+        need_next = true;
+        setTimeout(process_queue,100)
     }
-    var items = json['response']['items'];
-    var url,
-    date;
-    for (id in items) {
-      el.html('[' + phrases.wait + ' ' + count + '/' + json.response.count + ']');
-      url = items[id].sizes.slice(-1)[0].url;
-      date = settings.date.type ? new Date(items[id]['date'] * 1000).toString(settings.date.format)  : '';
-      if (settings.date.type == 2) images += date + '<br/>';
-      images += '<img alt="' + date + '" title="' + date + '" src="' + url + '" />';
-      if (settings.date.type == 2) images += '<br/>';
-      links += url + '<br/>';
-      count++;
-    }
-    if (count >= json.response.count) all = true;
-    loop++;
   }
-  el.html('<a class="vkopt-open-page">[' + phrases.open_page + ']</a><a class="vkopt-open-links">[' + phrases.get_links + ']</a>');
-  $(document).on('click', '.vkopt-open-page', function () {
-  openNew($('.photos_album_intro h1').html() + ' (owner id ' + owner + ', album id ' + album + ')', images);
-  return false;
-});
-$(document).on('click', '.vkopt-open-links', function () {
-  openNew($('.photos_album_intro h1').html() + ' (owner id ' + owner + ', album id' + album + ')', links);
-  return false;
-});
+  process_queue();
   return false;
 });
